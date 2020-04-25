184a0bc74669de9ccaa9f88eeccb6fba69db05e0
Marissol Karczeski <marfkar@gmail.com>
2020-04-25 14:46:22 -0700

    modify images

--- my-portfolio/src/components/projects.js
+++ my-portfolio/src/components/projects.js
@@ -1,65 +1,110 @@
 import React, { Component } from 'react';
-import { Grid, Cell} from 'react-mdl';
+import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
 
 
 class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+
+  toggleCategories() {
+
+    if(this.state.activeTab === 0){
+      return(
+        <div className="projects-grid">
+            
+          {/* Project 1 */}
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '400px', background: 'url(/images/recipe-search.png'}} >Project #1</CardTitle>
+            <CardText>
+              This was our first group project at UW coding bootcamp. An app to search for recipes based on ingredients that the user may have at home.
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Heroku</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* Project 2 */}
+          <Card shadow={5} style={{minWidth: '550', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #2</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Heroku</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+                  </Card>
+                  
+
+
+          Project 3
+          <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+            <CardTitle style={{color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover'}} >React Project #3</CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>GitHub</Button>
+              <Button colored>Heroku</Button>
+              <Button colored>Live Demo</Button>
+            </CardActions>
+            <CardMenu style={{color: '#fff'}}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+       
+</div>
+
+      )
+    } else if(this.state.activeTab === 1) {
+      return (
+        <div><h1>This is Express</h1></div>
+      )
+    } else if(this.state.activeTab === 2) {
+      return (
+        <div><h1>This is HTML/CSS</h1></div>
+      )
+    } else if(this.state.activeTab === 3) {
+      return (
+        <div><h1>This is MySQL/MongoDB</h1></div>
+      )
+    }
+
+  }
+
+
+
   render() {
     return(
-<div>
-              <Grid className="project-section-1">
-        <Cell col={6}>
-<img src="./images/recipe-search.png"
-alt="first-page website recipe search"
-className="recipe-search"/>
-<hr/>
-<a href="https://github.com/xzengeric/Project1" target="_blank">
-              <i className="fab fa-github-alt fa-2x"></i>
-            </a>
- <a href=" https://xzengeric.github.io/Project1/project1.html" target="_blank">
-              <i className="fab fa-github-alt fa-2x"></i>
-            </a>
-   
-</Cell>
+      <div>
+        <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+          <Tab>Group Projects</Tab>
+          <Tab>Express</Tab>
+          <Tab>HTML/CSS</Tab>
+          <Tab>MySQL/MongoDB</Tab>
+        </Tabs>
 
 
+          <Grid>
+            <Cell col={12}>
+              <div className="content">{this.toggleCategories()}</div>
+            </Cell>
+          </Grid>
 
 
-        <Cell col={6}>
-        <img src="./images/project-2.png"
-alt="first-page website connecting people"
-className="connect-people"/>
-</Cell>
-    </Grid>
-
-
-
-<Grid className="project-section-2">
-        <Cell col={6}>
-<img src="./images/recipe-search.png"
-alt="first-page website recipe search"
-className="recipe-search"/>
-<hr/>
-<a href="https://github.com/xzengeric/Project1" target="_blank">
-              <i className="fab fa-github-alt fa-2x"></i>
-            </a>
- <a href=" https://xzengeric.github.io/Project1/project1.html" target="_blank">
-              <i className="fab fa-github-alt fa-2x"></i>
-            </a>
-   
-</Cell>
-
-
-
-
-        <Cell col={6}>
-        <img src="./images/project-2.png"
-alt="first-page website connecting people"
-className="connect-people"/>
-</Cell>
-    </Grid>
-</div>
-      )
-    
+      </div>
+    )
   }
 }
 
