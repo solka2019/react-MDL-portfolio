fb0bd93d7b504ea878c27dfc825e615cf1e07192
Marissol Karczeski <marfkar@gmail.com>
2020-05-31 17:30:49 -0700

    Updates

---
+++ precache-manifest.1ce628e4e2166b0819b00510d003f3e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c303bdd4ed970f6d55e57e03e4d2d5dc",
+    "url": "/react-MDL-portfolio/index.html"
+  },
+  {
+    "revision": "73bb37ded58e90e3d302",
+    "url": "/react-MDL-portfolio/static/css/2.b6a8c48d.chunk.css"
+  },
+  {
+    "revision": "c56f9d5c4643a45d604a",
+    "url": "/react-MDL-portfolio/static/css/main.1a5097ed.chunk.css"
+  },
+  {
+    "revision": "73bb37ded58e90e3d302",
+    "url": "/react-MDL-portfolio/static/js/2.184e6ba3.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/react-MDL-portfolio/static/js/2.184e6ba3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56f9d5c4643a45d604a",
+    "url": "/react-MDL-portfolio/static/js/main.19ebe0b0.chunk.js"
+  },
+  {
+    "revision": "0896e65fe821f91e1a11",
+    "url": "/react-MDL-portfolio/static/js/runtime-main.ee3c8db7.js"
+  }
+]);
